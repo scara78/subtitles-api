b56da30b5741e9c14525023aeb26b14f5527bc12
nuzzesick <mati280341@gmail.com>
2021-12-06 20:32:41 -0300

    Fix when torrent was already added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import WebTorrent from 'webtorrent';
 import cors from 'cors';
 import express from 'express';
 import wrtc from 'wrtc';
+import parseTorrent from 'parse-torrent'
 import { announceList as defaultAnnounceList } from 'create-torrent';
 
 globalThis.WEBTORRENT_ANNOUNCE = defaultAnnounceList
@@ -32,12 +33,29 @@ app.use(cors());
 
 app.use(express.static(downloadsPath));
 
-app.post('/download', (req, res, next) => {
+const checkIfTorrentIsValid = (torrentId) => new Promise((resolve) => {
+  try {
+    parseTorrent(torrentId);
+    resolve(torrentId);
+  } catch {
+    parseTorrent.remote(torrentId, (err, parsedTorrent) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(parsedTorrent);
+      }
+    });
+  }
+});
+
+app.post('/download', async (req, res, next) => {
   try {
     const { id: torrentId } = req.body;
-    const torrentWasAdded = client.get(torrentId);
-    if (!torrentWasAdded) {
-      client.add(torrentId, { path: downloadsPath }, (t) => {
+    const torrentIsValid = await checkIfTorrentIsValid(torrentId);
+    const torrentHash = await torrentIsValid.infoHash;
+    const torrentWasAdded = client.get(await torrentHash);
+    if (!torrentWasAdded && torrentHash) {
+      client.add(await torrentHash, { path: downloadsPath }, (t) => {
 				const video = t.files.find((file) => file.name.endsWith('.mp4') || file.name.endsWith('.mkv'));
 				returnJSON({ req, res, next, code: 200, status: 'ok', message: 'Torrent downloading', magnet: t.magnetURI, video: video.path });
       });
