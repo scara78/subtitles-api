54e64c2f3597599daae90c03d89aadaebf0c80f8
nuzzesick <mati280341@gmail.com>
2021-12-06 02:49:06 -0300

    Fixes

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import WebTorrent from 'webtorrent';
 import cors from 'cors';
 import express from 'express';
-const app = express();
-const port = 8080;
 import wrtc from 'wrtc';
 import { announceList as defaultAnnounceList } from 'create-torrent';
 
@@ -11,6 +9,10 @@ globalThis.WEBTORRENT_ANNOUNCE = defaultAnnounceList
   .filter((url) => url.indexOf('wss://') === 0 || url.indexOf('ws://') === 0);
 
 globalThis.WRTC = wrtc;
+
+const app = express();
+const port = 8080;
+const downloadsPath = 'downloads';
 
 const client = new WebTorrent();
 
@@ -28,15 +30,17 @@ app.use(express.json());
 
 app.use(cors());
 
+app.use(express.static(downloadsPath));
 
 app.post('/download', (req, res, next) => {
   try {
     const { id: torrentId } = req.body;
     const torrentWasAdded = client.get(torrentId);
     if (!torrentWasAdded) {
-      client.add(torrentId, { path: 'downloads' }, (t) => {
-				t.createServer().listen(3000);
-				returnJSON({ req, res, next, code: 200, status: 'ok', message: 'Torrent downloading', magnet: t.magnetURI });
+      client.add(torrentId, { path: downloadsPath }, (t) => {
+				const video = t.files.find((file) => file.name.endsWith('.mp4'));
+				console.log(video.path)
+				returnJSON({ req, res, next, code: 200, status: 'ok', message: 'Torrent downloading', magnet: t.magnetURI, video: video.path });
       });
     } else {
       returnJSON({ req, res, next, code: 400, status: 'error', message: 'Torrent was already added' });
@@ -47,5 +51,5 @@ app.post('/download', (req, res, next) => {
 })
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Bitflix API listening at http://localhost:${port}`);
 });
