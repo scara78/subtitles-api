3a4f5c71486d4970e1a99405d93b1486166155a1
nuzzesick <mati280341@gmail.com>
2022-03-20 15:58:00 -0300

    bug fixing

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 import cors from 'cors';
 import express from 'express';
 import 'dotenv/config';
-import fs from 'fs';
-import srtToVtt from 'srt-to-vtt';
-import request from 'request';
-import { unzip } from 'zlib';
 import path from 'path';
 import OS from 'opensubtitles-api';
 
@@ -71,12 +67,9 @@ app.get('/subs/movie/:id', async (req, res, next) => {
   try {
     const { id: imdbid } = req.params;
     const subs = await OpenSubtitles.search({ imdbid, gzip: true });
-    if (Object.keys(subs).length === 0) {
-      returnJSON({ req, res, next, code: 400, status: 'error', message: 'Error while obtaining subtitles' });
-    } else {
-      returnFinalSubs({ req, res, next, imdbid, subs: await subs });
-    }
+    returnJSON({ req, res, next, code: 200, status: 'ok', imdbid, message: 'Subtitles obtained', subs: await subs });
   } catch (error) {
+    console.log(error)
     returnJSON({ req, res, next, code: 400, status:  'error', message: 'Unexpected error' });
   }
 })
@@ -85,11 +78,7 @@ app.get('/subs/tv/:id/:season/:episode', async (req, res, next) => {
   try {
     const { id: imdbid, season, episode } = req.params;
     const subs = await OpenSubtitles.search({ imdbid, season, episode, gzip: true });
-    if (Object.keys(subs).length === 0) {
-      returnJSON({ req, res, next, code: 400, status: 'error', message: 'Error while obtaining subtitles' });
-    } else {
-      returnFinalSubs({ req, res, next, imdbid: `${imdbid}-S${season}-E${episode}`, subs: await subs });
-    }
+    returnJSON({ req, res, next, code: 200, status: 'ok', subs });
   } catch (error) {
     returnJSON({ req, res, next, code: 400, status:  'error', message: 'Unexpected error' });
   }
